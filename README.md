01aae8b2d7b03a34a22c8c0588866c5e30543d37
Akandesh <Akandessh@live.com>
2022-06-03 01:43:53 +0200

    Update: Fri Jun  3 01:43:53 WEDT 2022

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 🚀 Up to date offsets for Rust. Automatically kept up to date thanks to [rust-auto-dumper](https://github.com/Akandesh/rust-auto-dumper).
 
 
-### Last Update 02-06-2022 20:05:13
+### Last Update 03-06-2022 01:43:48
 - If there's something missing then open a issue and i'll try to add it asap.
--- rust.cs
+++ rust.cs
@@ -2,15 +2,15 @@ using System;
 namespace blazedumper {
     public static class BaseEntity_TypeInfo
     {
-    public const Int32 offset = 0x3216C50;
+    public const Int32 offset = 0x3216C48;
     }
     public static class MainCamera_TypeInfo
     {
-    public const Int32 offset = 0x3218808;
+    public const Int32 offset = 0x32187F0;
     }
     public static class Facepunch_Input_TypeInfo
     {
-    public const Int32 offset = 0x3228F40;
+    public const Int32 offset = 0x3228F48;
     }
     public static class BasePlayer
     {
