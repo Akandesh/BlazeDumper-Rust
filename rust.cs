7ace63a7669c519fddb7b9a71060ab0717c24d64
Akandesh <Akandessh@live.com>
2022-07-07 19:57:40 +0200

    Update: Thu Jul  7 19:57:40 WEDT 2022

--- rust.cs
+++ rust.cs
@@ -2,15 +2,15 @@ using System;
 namespace blazedumper {
     public static class BaseEntity_TypeInfo
     {
-    public const Int32 offset = 0x3217C48;
+    public const Int32 offset = 0x323E538;
     }
     public static class MainCamera_TypeInfo
     {
-    public const Int32 offset = 0x32197F8;
+    public const Int32 offset = 0x3240138;
     }
     public static class Facepunch_Input_TypeInfo
     {
-    public const Int32 offset = 0x3229F30;
+    public const Int32 offset = 0x3250938;
     }
     public static class BasePlayer
     {
