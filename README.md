0c7c429c3fbbfe56a482fb2fcdc799d8709e45b6
Akandesh <Akandessh@live.com>
2023-01-11 13:22:09 +0100

    Update: Wed Jan 11 13:22:09 WEST 2023

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 🚀 Up to date offsets for Rust. Automatically kept up to date thanks to [rust-auto-dumper](https://github.com/Akandesh/rust-auto-dumper).
 
 
-### Last Update 09-01-2023 17:42:30
+### Last Update 11-01-2023 13:22:04
 - If there's something missing then open a issue and i'll try to add it asap.
--- rust.h
+++ rust.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x3655780;
-    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x3670928;
-    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x3657938;
-    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x3654AF0;
-    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x3654B90;
+    constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x36567E0;
+    constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x3671990;
+    constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x36589A0;
+    constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x3655B50;
+    constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x3655BF0;
     namespace BasePlayer {
         constexpr ::std::ptrdiff_t __menuOption_Climb = 0x240; // Option
         constexpr ::std::ptrdiff_t __menuOption_Drink = 0x298; // Option
