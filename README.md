a289f936918f303a5f6978d6123581af9f5ac285
Akandesh <Akandessh@live.com>
2023-02-22 10:42:18 +0100

    Update: Wed Feb 22 10:42:18 WEST 2023

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 🚀 Up to date offsets for Rust. Automatically kept up to date thanks to [rust-auto-dumper](https://github.com/Akandesh/rust-auto-dumper).
 
 
-### Last Update 21-02-2023 22:14:01
+### Last Update 22-02-2023 10:42:13
 - If there's something missing then open a issue and i'll try to add it asap.
--- rust_inline.h
+++ rust_inline.h
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 namespace blazedumper {
-    inline constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x2EAC938;
-    inline constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x2EF8F20;
-    inline constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x2F0A650;
-    inline constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x2EE9858;
-    inline constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x2EACBA8;
+    inline constexpr ::std::ptrdiff_t BaseEntity_TypeInfo = 0x2EAC818;
+    inline constexpr ::std::ptrdiff_t Facepunch_Input_TypeInfo = 0x2EF8E68;
+    inline constexpr ::std::ptrdiff_t MainCamera_TypeInfo = 0x2F0A598;
+    inline constexpr ::std::ptrdiff_t System_Collections_Generic_List_BaseGameMode_TypeInfo = 0x2EE9790;
+    inline constexpr ::std::ptrdiff_t BaseGameMode_TypeInfo = 0x2EACA88;
     namespace BasePlayer {
         inline constexpr ::std::ptrdiff_t __menuOption_Climb = 0x240; // Option
         inline constexpr ::std::ptrdiff_t __menuOption_Drink = 0x298; // Option
